153402a963ac6eb7cf3820f167c9d30060f22aef
Johannes Gorset <jgorset@gmail.com>
2011-04-20 11:08:15 +0200

    Fix view to reflect README

--- views.py
+++ views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
+from fandjango.decorators import facebook_authorization_required
+
+@facebook_authorization_required()
 def home(request):
-    return HttpResponse('Hello, %s' % request.facebook.user.first_name)
+    return HttpResponse('Hello, %s.' % request.facebook.user.first_name)
